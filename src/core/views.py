5eb830b9529487dbaf6c2d88b506079640ebdb03
Samurai1986 <vlad.sidorenko@mail.ru>
2017-05-21 19:56:40 +0500

    correct
    Http response done

--- src/core/views.py
+++ src/core/views.py
@@ -9,10 +9,10 @@ from django.views.decorators.csrf import csrf_exempt
 def urls(request):
     if request.method == 'POST': #savelink
         print("it's post")
-        return render("it\'s post")
+        return HttpResponse("it\'s post")
     elif request.method == 'GET': #returnlink
         print('it\'s get')
-        return render('it\'s get')
+        return HttpResponse('it\'s get')
 
         #self.shlink = ''.join(random.choice(string.ascii_uppercase + string.digits) for _ in range(Shortener.shlink.max_length)) #random code generator
         #self.save
