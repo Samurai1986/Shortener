5956cd8829ecaaa0bb8cb268e4e9ae158b47aaf9
Samurai1986 <vlad.sidorenko@mail.ru>
2017-05-21 23:08:32 +0500

    savelink test completed
    Post method is completed

--- src/core/models.py
+++ src/core/models.py
@@ -4,3 +4,4 @@ from django.db import models
 class Shortener(models.Model):
     link = models.URLField(max_length=250)
     shlink = models.CharField(max_length=5)
+    length = shlink.max_length
--- src/core/views.py
+++ src/core/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
+#from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Shortener
+from django.contrib.sites.shortcuts import get_current_site
 import random, string
 from django.views.decorators.csrf import csrf_exempt
 # Create your views here.
@@ -8,13 +9,17 @@ from django.views.decorators.csrf import csrf_exempt
 @csrf_exempt
 def urls(request):
     if request.method == 'POST': #savelink
-        print("it's post")
-        return HttpResponse("it\'s post")
+        print(request)
+        Shortener.link = request
+        Shortener.shlink = ''.join(random.choice(string.ascii_uppercase + string.digits) for _ in range(Shortener.length)) #random code generator
+        Shortener.save
+        response  = get_current_site(request),"/",Shortener.shlink
+        return HttpResponse(response)
     elif request.method == 'GET': #returnlink
         print('it\'s get')
         return HttpResponse('it\'s get')
 
-        #self.shlink = ''.join(random.choice(string.ascii_uppercase + string.digits) for _ in range(Shortener.shlink.max_length)) #random code generator
+        #
         #self.save
 
 #def returnlink(request):
